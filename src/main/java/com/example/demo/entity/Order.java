6ea5ee1d76c99f613c12520538e3bfd98fd8ef84
Асель Абдрахманова <dellya4>
2026-04-06 01:57:15 +0500

    feat: add User and Order class, add enum Product

--- src/main/java/com/example/demo/entity/Order.java
+++ src/main/java/com/example/demo/entity/Order.java
@@ -1,9 +1,11 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Table
 @Entity
@@ -11,4 +13,19 @@ import lombok.Setter;
 @Setter
 
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Product product;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,4 +1,24 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table
+@Entity
+@Getter
+@Setter
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String city;
 }
